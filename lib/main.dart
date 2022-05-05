74c19a6c0a808e9cb7bbc19fbc48946f59e091d7
Matija Marolin <matija.marolin11@gmail.com>
2022-05-05 10:45:11 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shoppy',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shoppy'),
+      ),
+      body: Center(
+        child: Text('Welcome to my shop boi.'),
+      ),
+    );
+  }
+}
